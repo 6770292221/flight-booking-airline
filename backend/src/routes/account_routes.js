f65bcfa7f82c7f263e9410aef399f8cc7b49098d
6770292221 <6770292221@student.chula.ac.th>
2025-02-19 17:18:17 +0700

    rename middleware function

--- backend/src/routes/account_routes.js
+++ backend/src/routes/account_routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { createAccount, verifyUser } from "../controllers/account_controller.js";
-import { verifyToken } from "../middleware/auth.js";
+import { getTokenData } from "../middleware/auth.js";
 
 
 const routerAccount = Router();
 
 routerAccount.post("/register", createAccount);
-routerAccount.post("/verifyUser", verifyToken, verifyUser);
+routerAccount.post("/verifyUser", getTokenData, verifyUser);
 
 
 export default routerAccount;
--- backend/src/routes/auth_routes.js
+++ backend/src/routes/auth_routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { loginUser, logoutUser, verify2fa } from "../controllers/auth_controller.js";
-import { verifyToken, verify2faToken } from "../middleware/auth.js";
+import { verifyToken, getTokenData } from "../middleware/auth.js";
 const routerAuth = Router();
 
 routerAuth.post("/auth/login", loginUser);
 routerAuth.post("/auth/logout", verifyToken, logoutUser);
-routerAuth.post('/auth/verify2fa', verify2faToken, verify2fa);
+routerAuth.post('/auth/verify2fa', getTokenData, verify2fa);
 
 
 export default routerAuth;
