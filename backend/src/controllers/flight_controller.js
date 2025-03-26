ff8bb8eabb2ad7588b8b5f6f492339cfb52dff20
Titi Changpoo <70009215@krungsri.com>
2025-03-27 00:23:41 +0700

    reverse code for sending email

--- backend/src/controllers/flight_controller.js
+++ backend/src/controllers/flight_controller.js
@@ -4,6 +4,8 @@ import { AirportMongooseModel } from "../models/airport_models.js";
 import { CabinClassMongooseModel } from "../models/cabin_models.js";
 import { HeaderInterceptor } from "../utils/header_interceptor.js";
 import { MapUtils } from "../utils/map_utils.js";
+import {FlightSearchContext} from "../state/flight_search_context.js"
+import {OnewayState,RoundtripState } from "../state/flight_search_state.js"
 import axios from "axios";
 
 export const postFlightsOffer = async (req, res) => {
@@ -14,52 +16,28 @@ export const postFlightsOffer = async (req, res) => {
     const domestic = airlines.map((value) => value.carrierCode).join(',')
     const aircrafts = await AircraftMongooseModel.find();
     const cabins = await CabinClassMongooseModel.find();
-    let resp = {
-      dircetion: '',
-      outbound: [],
-      inbound: []
-    };
+    const context = new FlightSearchContext();
 
-    let { direction } = req.body;
-      if( direction == "ONEWAY" ) {
-        resp.dircetion = "ONEWAY"
+    switch (req.body.direction) {
+      case "ONEWAY":
+        context.setState(new OnewayState());
+        break;
+      case "ROUNDTRIP":
+        context.setState(new RoundtripState());
+        break;
+      default:
+        return res.status(400).json({ error: "Invalid direction" });
+    }
 
-        const config = HeaderInterceptor.setConfigOffer(req.body, domestic, true);
-        const respOutbound = await axios.request(config);
-        resp.outbound = MapUtils.createMappedFlightDetails(
-          respOutbound.data.data,
-          "OUTBOUND",
-          airlines,
-          airports,
-          aircrafts,
-          cabins
-        );
-      } else if (direction == "ROUNDTRIP") {
-        resp.dircetion = "ROUNDTRIP"
-        let config =  HeaderInterceptor.setConfigOffer(req.body, domestic, true);
+    const response = await context.search(req.body, {
+      airlines,
+      airports,
+      aircrafts,
+      cabins,
+      domestic
+    });
 
-        const respOutbound = await axios.request(config);
-        resp.outbound = MapUtils.createMappedFlightDetails(
-          respOutbound.data.data,
-          "OUTBOUND",
-          airlines,
-          airports,
-          aircrafts,
-          cabins
-        );
-        config =  HeaderInterceptor.setConfigOffer(req.body, domestic, false);
-        const respInbound = await axios.request(config);
-        resp.inbound = MapUtils.createMappedFlightDetails(
-          respInbound.data.data,
-          "INBOUND",
-          airlines,
-          airports,
-          aircrafts,
-          cabins
-        );
-
-      }
-      res.status(200).json(resp)
+      res.status(200).json(response)
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
