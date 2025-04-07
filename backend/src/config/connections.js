10d203eee080c455c211874416ccb2d8c01b858c
Krissada S <>
2025-04-07 23:12:26 +0700

    refactor(config): removes hardcoded database credentials and uses process.env instead

--- backend/src/config/connections.js
+++ backend/src/config/connections.js
@@ -1,18 +1,33 @@
 import mongoose from "mongoose";
+import logger from "../utils/logger_utils.js";
 
-export const flightDb = mongoose.createConnection(
-    "mongodb+srv://imaikungki:1wS7n7lGvEyiSo66@cluster0.r662p.mongodb.net/flight?retryWrites=true&w=majority"
-);
+function createDbConnection(envVarName, dbName) {
+  const connectionString = process.env[envVarName];
 
-export const userDb = mongoose.createConnection(
-    "mongodb+srv://imaikungki:1wS7n7lGvEyiSo66@cluster0.r662p.mongodb.net/user?retryWrites=true&w=majority"
-);
+  if (!connectionString) {
+    logger.error(`Environment variable ${envVarName} is not set. Cannot create connection for ${dbName}.`);
+    throw new Error(`Configuration Error: Environment variable ${envVarName} is required for ${dbName}.`);
+  }
 
+  logger.info(`Creating connection for ${dbName} using environment variable ${envVarName}`);
+  const connection = mongoose.createConnection(connectionString);
 
-export const paymentDb = mongoose.createConnection(
-    "mongodb+srv://imaikungki:1wS7n7lGvEyiSo66@cluster0.r662p.mongodb.net/payment?retryWrites=true&w=majority"
-);
+  connection.on('connected', () => {
+    logger.info(`[${dbName}] MongoDB connected successfully.`);
+  });
 
-export const bookingDb = mongoose.createConnection(
-    "mongodb+srv://imaikungki:1wS7n7lGvEyiSo66@cluster0.r662p.mongodb.net/booking?retryWrites=true&w=majority"
-);
+  connection.on('error', (error) => {
+    logger.error(`[${dbName}] MongoDB connection error:`, error);
+  });
+
+  connection.on('disconnected', () => {
+    logger.warn(`[${dbName}] MongoDB disconnected.`);
+  });
+
+  return connection;
+}
+
+export const flightDb = createDbConnection("FLIGHT_DB_URI", "Flight DB");
+export const userDb = createDbConnection("USER_DB_URI", "User DB");
+export const paymentDb = createDbConnection("PAYMENT_DB_URI", "Payment DB");
+export const bookingDb = createDbConnection("BOOKING_DB_URI", "Booking DB");
