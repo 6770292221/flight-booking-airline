6a6dc4cdcc592a02d90fffeb88ee77daf43e3f4e
titic443 <titi.changpoo@gmail.com>
2025-04-07 16:33:26 +0700

    singleton: redis service

--- backend/src/utils/redis_utils.js
+++ backend/src/utils/redis_utils.js
@@ -1,27 +1,47 @@
-import redis from 'redis';
-import dotenv from 'dotenv';
-import logger from '../utils/logger_utils.js';
+// utils/redis_client.js
+import redis from "redis";
+import dotenv from "dotenv";
+import logger from "../utils/logger_utils.js";
+
 dotenv.config({ path: "./src/config/config.env" });
 
+class RedisClient {
+    static instance;
 
-const client = redis.createClient({
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    },
-    password: process.env.REDIS_PASSWORD,
-});
+    constructor() {
+        if (RedisClient.instance) {
+            return RedisClient.instance;
+        }
 
+        this.client = redis.createClient({
+            socket: {
+                host: process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT,
+            },
+            password: process.env.REDIS_PASSWORD,
+        });
 
+        this.client.on("connect", () => {
+            logger.info("Redis connected successfully");
+        });
 
-client.on('connect', () => {
-    logger.info('Redis connected successfully');
-});
+        this.client.on("error", (err) => {
+            logger.error("Redis connection failed: " + err);
+        });
 
-client.on('error', (err) => {
-    logger.error('Redis connection failed:' + err);
-});
+        RedisClient.instance = this;
+    }
 
+    static getInstance() {
+        if (!RedisClient.instance) {
+            RedisClient.instance = new RedisClient();
+        }
+        return RedisClient.instance;
+    }
 
-export default client;
+    getClient() {
+        return this.client;
+    }
+}
 
+export default RedisClient.getInstance().getClient();
