f175ab1238b631434eedd02e2cfe6753dea8157f
imaikungki <aphirak_2008@hotmail.com>
2025-06-25 01:16:22 +0700

    data-testid

--- backend/src/utils/passport.js
+++ backend/src/utils/passport.js
@@ -6,7 +6,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/v1/user-core-api/auth/google/callback",
+        callbackURL: "https://flight-booking-airline.onrender.com/api/v1/user-core-api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
